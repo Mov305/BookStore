bc6573c0b1e4d5ff4507cfb66bb64468912c2fc4
Mov305 <mov3three2001@gmail.com>
2022-09-19 23:48:13 +0200

    added all the functionalities

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -1,45 +1,68 @@
-const Book = () => (
-  <li className="relative flex flex-col items-center p-8 overflow-hidden border bg-white border-gray-200 rounded-lg md:flex-row md:justify-between dark:bg-gray-800 ">
-    <span className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600" />
-    <div className="flex flex-col items-center md:items-start">
-      <span className="mt-1 text-lg text-gray-500 font-semibold dark:text-gray-300 ">
-        Science Fiction
-      </span>
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/books';
 
-      <h5 className="text-xl font-bold text-gray-900 dark:text-slate-50">Book name</h5>
-      <span className="text-[#08ACB4] font-thin dark:font-normal">Author</span>
-      <div className="flex text-[#08ACB4] py-4">
-        <button type="button" className=" border-r-4 border-[#E8E8E8] dark:border-slate-400 pr-3">
-          Comments
-        </button>
-        <button type="button" className=" border-r-4 border-[#E8E8E8] dark:border-slate-400 px-3">
-          Edit
-        </button>
-        <button type="button" className="px-3">
-          Remove
-        </button>
+const Book = ({ book }) => {
+  const {
+    title,
+    author,
+    category,
+    progress,
+    chapter,
+    id,
+  } = book;
+  const dispatch = useDispatch();
+
+  const handleRemove = (e) => {
+    e.preventDefault();
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <li className="relative flex flex-col items-center p-8 overflow-hidden border bg-white border-gray-200 rounded-lg md:flex-row md:justify-between dark:bg-gray-800 ">
+      <span className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600" />
+      <div className="flex flex-col items-center md:items-start md:flex-grow">
+        <span className="mt-1 text-lg text-gray-500 font-semibold dark:text-gray-300 ">
+          {category}
+        </span>
+
+        <h5 className="text-xl font-bold text-gray-900 dark:text-slate-50">{title}</h5>
+        <span className="text-[#08ACB4] font-thin dark:font-normal">{author}</span>
+        <div className="flex text-[#08ACB4] py-4">
+          <button type="button" className=" border-r-4 border-[#E8E8E8] dark:border-slate-400 pr-3">
+            Comments
+          </button>
+          <button type="button" className=" border-r-4 border-[#E8E8E8] dark:border-slate-400 px-3">
+            Edit
+          </button>
+          <button type="button" className="px-3" onClick={handleRemove}>
+            Remove
+          </button>
+        </div>
       </div>
-    </div>
-    <div className="mt-4  md:pr-16 md:border-r-2 md:border-[#E8E8E8] dark:md:border-slate-400 py-5 ">
-      <div>
-        <div
-          style={{ background: 'conic-gradient(#08ACB4 300deg, #E8E8E8 0deg )' }}
-          className=" w-28 h-28 rounded-full flex justify-center items-center "
-        >
-          <div className=" w-24 h-24 rounded-full bg-white dark:text-gray-100 dark:bg-gray-800 flex justify-center items-center">
-            <span className="text-2xl">80%</span>
+      <div className="mt-4  md:pr-6 lg:pr-16 md:border-r-2 md:border-[#E8E8E8] dark:md:border-slate-400 py-5 ">
+        <div>
+          <div
+            style={{ background: `conic-gradient(#08ACB4 ${progress * 3.6}deg, #E8E8E8 0deg )` }}
+            className=" w-28 h-28 rounded-full flex justify-center items-center "
+          >
+            <div className=" w-24 h-24 rounded-full bg-white dark:text-gray-100 dark:bg-gray-800 flex justify-center items-center">
+              <span className="text-2xl">
+                {progress}
+                %
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="md:ml-10 flex flex-col items-center md:items-start text-center md:text-left">
-      <p className="text-gray-500 dark:text-gray-200 font-thin">CURRENT CHAPTER</p>
-      <h4 className="dark:text-gray-100">Chapter 3: "A Lesson Learned"</h4>
-      <button type="button" className="bg-[#08ACB4] text-white p-2 rounded-md mt-2 md:mt-10">
-        UPDATE PROGRESS
-      </button>
-    </div>
-  </li>
-);
+      <div className="md:ml-10 flex flex-col items-center md:items-start text-center md:text-left md:w-52">
+        <p className="text-gray-500 dark:text-gray-200 font-thin">CURRENT CHAPTER</p>
+        <h4 className="dark:text-gray-100">{chapter}</h4>
+        <button type="button" className="bg-[#08ACB4] text-white p-2 rounded-md mt-2 md:mt-10">
+          UPDATE PROGRESS
+        </button>
+      </div>
+    </li>
+  );
+};
 
 export default Book;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,48 @@ export const ADD_BOOK = 'ADD_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
 
 // state
-const initialState = [];
+const initialBook = {
+  id: 0,
+  title: '',
+  author: '',
+  category: '',
+  progress: 0,
+  chapter: 'Chapter 1',
+};
+const initialState = [
+  {
+    id: 1,
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    category: 'Action',
+    progress: 85,
+    chapter: 'Chapter 17: The Hunger Games',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+    category: 'Sci-Fi',
+    progress: 10,
+    chapter: 'Chapter 1: Arrakis',
+  },
+  {
+    id: 3,
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    category: 'Learning',
+    progress: 70,
+    chapter: 'Introduction',
+  },
+  {
+    id: 4,
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    category: 'Action',
+    progress: 50,
+    chapter: 'The Fellowship of the Ring',
+  },
+];
 
 // action creators
 export const addBook = (book) => ({
@@ -21,7 +62,7 @@ export default function booksReducer(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case ADD_BOOK:
-      return [...state, payload];
+      return [...state, { ...initialBook, ...payload }];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== payload);
     default:
